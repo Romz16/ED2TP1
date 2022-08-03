6dffeb91146b2e83e0d691f5561b829142090068
Mateus Filipe <mateusfilipe2k2@gmail.com>
2022-08-03 13:03:02 -0300

    inicial

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <ctype.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+pesquisa <método> <quantidade> <situação> <chave> [-P]
+
+<método>  representa  o  método  de  pesquisa  externa  a  ser  executado,  podendo  ser  um  número 
+inteiro de 1 a 4, de acordo com a ordem dos métodos mencionados; 
+
+<quantidade> representa a quantidade de registros do arquivo considerado; 
+
+<situação> representa a situação de ordem do arquivo, podendo ser 1 (arquivo ordenado 
+ascendentemente), 2 (arquivo ordenado descendentemente) ou 3 (arquivo desordenado 
+aleatoriamente); 
+
+<chave> representa a chave a ser pesquisada no arquivo considerado; 
+
+[-P] representa um argumento opcional que deve ser colocado quando se deseja que as chaves de 
+pesquisa dos registros do arquivo considerado sejam apresentadas na tela.
+*/
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+}
